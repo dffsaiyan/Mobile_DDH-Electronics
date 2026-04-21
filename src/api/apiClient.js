01c8991e6d38e12a484991f9cacc66c9ba824639
Duongng06 <duongnguyen0602ls@gmail.com>
2026-04-21 19:20:55 +0700

    Di chuyen sang dung Cloudflare tunnel

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,16 +2,14 @@ import axios from 'axios';
 
 // 🤖 FILE NÀY ĐƯỢC CẬP NHẬT TỰ ĐỘNG MỖI KHI CHẠY 'NPM START'
 // 🤖 FILE NÀY ĐƯỢC CẬP NHẬT TỰ ĐỘNG MỖI KHI CHẠY 'NPM START'
-export const BASE_URL = 'https://ddh-electronics.loca.lt/api'; 
-export const IMAGE_BASE_URL = 'https://ddh-electronics.loca.lt';
+export const BASE_URL = 'https://ddh-electronics.powercapital-hk.space/api'; 
+export const IMAGE_BASE_URL = 'https://ddh-electronics.powercapital-hk.space';
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'Bypass-Tunnel-Reminder': 'true', // Header quan trọng để bypass trang cảnh báo của Localtunnel
+    'Content-Type': 'application/json',
   },
 });
 
